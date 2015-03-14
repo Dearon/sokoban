fdadabcdf44c15abe5ecc8225124de07ca465ec1
Dearon <dearon@gmail.com>
2015-03-14 13:48:44 +0100

    Added Immutable.js for the immutable types (and removing the need for lodash in the process) and player movement

--- src/js/sokoban.js
+++ src/js/sokoban.js
@@ -1,22 +1,96 @@
-var _ = require('lodash');
+var Immutable = require('immutable');
 var $ = require('domtastic');
+var Combokeys = require('Combokeys');
+var PubSub = require('pubsub-js');
 
-var stage = [
-    ['#', '#', '#', '#', '#', '#', '#'],
-    ['#', '.', '.', '.', '.', '.', '#'],
-    ['#', '.', '.', '@', '.', '.', '#'],
-    ['#', '.', '.', '.', '.', '.', '#'],
-    ['#', '#', '#', '#', '#', '#', '#']
-];
+var combokeys = new Combokeys(document);
 
-var plainStage = function(stage) {
-    return _.reduce(stage, function(result, row) {
-        var plainRow = _.reduce(row, function(result, field) {
-            return result + field;
-        }, '');
+var stage = Immutable.List([
+    Immutable.List(['#', '#', '#', '#', '#', '#', '#']),
+    Immutable.List(['#', '.', '.', '.', '.', '.', '#']),
+    Immutable.List(['#', '.', '.', '.', '.', '.', '#']),
+    Immutable.List(['#', '.', '.', '.', '.', '.', '#']),
+    Immutable.List(['#', '#', '#', '#', '#', '#', '#'])
+]);
 
-        return result + plainRow + '<br>';
-    }, '');
+var items = Immutable.Map({
+    player: Immutable.Map({
+        symbol: '@',
+        x: 2,
+        y: 3
+    })
+});
+
+var buildState = function(stage, items) {
+    var state = stage;
+
+    items.every(function(item) {
+        var row = state.get(item.get('x'));
+        row = row.set(item.get('y'), item.get('symbol'));
+        state = state.set(item.get('x'), row);
+    });
+
+    draw(state);
 };
 
-$('.sokoban').html(plainStage(stage));
+var draw = function(stage) {
+    var html = stage.reduce(function(html, row) {
+        var rowHtml = row.reduce(function(html, field) {
+            return html + field;
+        }, '');
+        
+        return html + rowHtml + '<br>';
+    }, '');
+
+    $('.sokoban').html(html);
+};
+
+buildState(stage, items);
+
+var handleInput = function(msg, data) {
+    var player = items.get('player');
+    var x = player.get('x');
+    var y = player.get('y');
+
+    if (data === 'left') {
+        y -= 1;
+    }
+
+    if (data === 'right') {
+        y += 1;
+    }
+
+    if (data === 'up') {
+        x -= 1;
+    }
+
+    if (data === 'down') {
+        x += 1;
+    }
+
+    if (stage.get(x).get(y) != '#') {
+        player = player.set('x', x);
+        player = player.set('y', y);
+        items = items.set('player', player);
+    }
+
+    buildState(stage, items);
+};
+
+PubSub.subscribe('input', handleInput);
+
+combokeys.bind('left', function() {
+    PubSub.publish('input', 'left');
+});
+
+combokeys.bind('right', function() {
+    PubSub.publish('input', 'right');
+});
+
+combokeys.bind('up', function() {
+    PubSub.publish('input', 'up');
+});
+
+combokeys.bind('down', function() {
+    PubSub.publish('input', 'down');
+});
